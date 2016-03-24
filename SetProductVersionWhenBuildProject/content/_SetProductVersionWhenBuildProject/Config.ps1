93ea26072c53877a5008d450e67173aef6ef8742
Eric Chen <echen@mdsol.com>
2016-03-24 12:42:42 -0400

    Updated based on comments.

--- SetProductVersionWhenBuildProject/content/_SetProductVersionWhenBuildProject/Config.ps1
+++ SetProductVersionWhenBuildProject/content/_SetProductVersionWhenBuildProject/Config.ps1
@@ -8,7 +8,8 @@ $githash = git log -1 --pretty='%h'
 # Product version value in the dll file property, which is set by AssemblyInformationalVersionAttribute in AssemblyInfo.cs
 $productVersionValue = "$githash$(if($(git status -s)){'-dirty'}else{''})"
 
-# Product version value to be used when building in MyGet Build Services
+# Product version value using environment variables available when building in MyGet Build Services.
+# http://docs.myget.org/docs/reference/build-services#Available_Environment_Variables
 $productVersionValueMyGet = "$($env:VersionFormat)-build$($env:BuildCounter)-$githash"
 
 # If keep below CSharp code in AssemblyInfo.cs after build
@@ -16,4 +17,4 @@ $productVersionValueMyGet = "$($env:VersionFormat)-build$($env:BuildCounter)-$gi
 $keepAssemblyInfoChangeAfterBuild = $false
 
 # Regex pattern for Product Version in AssemblyInfo.cs
-$productVersionPattern = '\s*\[assembly *: *AssemblyInformationalVersion *\(@?"[^"]*"\) *\]'
+$productVersionPattern = '\s*\[assembly\s*:\s*AssemblyInformationalVersion\s*\(@?"[^"]*"\)\s*\]'
